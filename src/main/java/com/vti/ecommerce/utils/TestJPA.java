4602bb6f63e1dafb623bdaf737e840e6414c32b0
bobbibao <lehoangbao1125@gmail.com>
2024-05-22 08:23:58 +0700

    Enhance project ang update dependencies

--- src/main/java/com/vti/ecommerce/utils/TestJPA.java
+++ src/main/java/com/vti/ecommerce/utils/TestJPA.java
@@ -1,11 +1,16 @@
 package com.vti.ecommerce.utils;
 
+import com.vti.ecommerce.domains.entities.Product;
+
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class TestJPA {
 	public static void main(String[] args) {
-		 EntityManagerFactory em = Persistence.createEntityManagerFactory("jpa-hibernate-mysql");
-		
+		 EnManagerFactory em = new EnManagerFactory();
+		 EntityManager entityManager = em.getEntityManager();
+		 Product p = entityManager.find(Product.class, 205082351);
+		 System.out.println(p);
 	}
 }
