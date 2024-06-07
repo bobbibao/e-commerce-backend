ca70b42e74f157dfc40e133cfe9ebe4ce2f945c8
bobbibao <lehoangbao1125@gmail.com>
2024-06-07 23:41:35 +0700

    Implement findByEmail in User repository

--- src/main/java/com/vti/ecommerce/repositories/IUserRepository.java
+++ src/main/java/com/vti/ecommerce/repositories/IUserRepository.java
@@ -1,10 +1,20 @@
 package com.vti.ecommerce.repositories;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.vti.ecommerce.domains.entities.Order;
 import com.vti.ecommerce.domains.entities.User;
 
-public interface UserRepository {
-	User findById(String userID);
-	User findByUserName(String userName);
-	boolean save(User user);
-	boolean deleteById(String userID);
+@Repository
+public interface IUserRepository extends JpaRepository<User, String>{
+//	User findById(String userID);
+	User findByEmail(String email);
+//	boolean save(User user);
+//	boolean deleteById(String userID);
+	
 }
