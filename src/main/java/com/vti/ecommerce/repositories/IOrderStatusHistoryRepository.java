fe72fcb57d20916cbc694ab91191c20061df65a7
bobbibao <lehoangbao1125@gmail.com>
2024-06-07 23:43:45 +0700

    Implement findByOrderId in order status history

--- src/main/java/com/vti/ecommerce/repositories/IOrderStatusHistoryRepository.java
+++ src/main/java/com/vti/ecommerce/repositories/IOrderStatusHistoryRepository.java
@@ -1,9 +1,15 @@
 package com.vti.ecommerce.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.vti.ecommerce.domains.entities.OrderStatusHistory;
 
+@Repository
 public interface IOrderStatusHistoryRepository extends JpaRepository<OrderStatusHistory, Long>{
-
+    @Query("SELECT o FROM OrderStatusHistory o WHERE o.order.orderID = :orderId")
+    Set<OrderStatusHistory> findByOrderId(Long orderId);
 }
