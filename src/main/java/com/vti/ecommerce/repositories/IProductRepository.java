cffad2d0c3775dde5a8fa857483f03fabbefca69
bobbibao <lehoangbao1125@gmail.com>
2024-06-07 23:39:34 +0700

    Add some method to product repository

--- src/main/java/com/vti/ecommerce/repositories/IProductRepository.java
+++ src/main/java/com/vti/ecommerce/repositories/IProductRepository.java
@@ -2,16 +2,19 @@ package com.vti.ecommerce.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.vti.ecommerce.domains.entities.Product;
-import com.vti.ecommerce.services.dto.ProductDto;
 
 
-
-public interface ProductRepository {
-	Product findById(Long productID);
-	ProductDto findById(String productID);
-	List<Product> findAll();
-	boolean save(Product product);
-	boolean deleteById(Long productID);
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Long>{
+//	Product findById(Long productID);
+//	ProductDto findById(String productID);
+//	List<Product> findAll();
+//	boolean save(Product product);
+//	boolean deleteById(Long productID);
+    List<Product> findByProductNameContaining(String search);
 	
 }
