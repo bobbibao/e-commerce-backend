c4459d5695f038c15e96681a9db4f372a715a23e
bobbibao <lehoangbao1125@gmail.com>
2024-06-14 22:50:09 +0700

    Implement update user's role

--- src/main/java/com/vti/ecommerce/services/IUserService.java
+++ src/main/java/com/vti/ecommerce/services/IUserService.java
@@ -1,20 +1,20 @@
 package com.vti.ecommerce.services;
 
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import com.vti.ecommerce.domains.entities.Order;
 import com.vti.ecommerce.domains.entities.User;
 import com.vti.ecommerce.services.dto.OrderDto;
+import com.vti.ecommerce.services.dto.UserDto;
 
-public interface IUserService extends UserDetailsService, IBaseService<User, Long>{
+public interface IUserService extends UserDetailsService, IBaseService<UserDto, String>{
 //    User getUser(String id);
 //    User createUser(User User);
 //    User updateUser(Long id, User User);
 //    boolean deleteUser(Long id);
     User getUserByEmail(String email);
     List<OrderDto> getOrdersByEmail(String email);
+    String getUserID(String email);
+    UserDto updateUserRole(String id, String role);
 }
