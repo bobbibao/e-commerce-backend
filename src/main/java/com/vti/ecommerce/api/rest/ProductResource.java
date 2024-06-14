dafcd3266423b613aa8558992b2800292d674bfa
bobbibao <lehoangbao1125@gmail.com>
2024-06-14 22:48:50 +0700

    Implement get product by page + limit and Put product

--- src/main/java/com/vti/ecommerce/api/rest/ProductResource.java
+++ src/main/java/com/vti/ecommerce/api/rest/ProductResource.java
@@ -3,27 +3,34 @@ package com.vti.ecommerce.api.rest;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vti.ecommerce.domains.entities.Product;
 import com.vti.ecommerce.services.IProductService;
 import com.vti.ecommerce.services.dto.ProductDto;
 
 @RestController
-@RequestMapping("/api/products")
+@CrossOrigin(origins = "*")
+@RequestMapping("/products")
 public class ProductResource {
 
 	private final IProductService productService;
 
 	public ProductResource(IProductService productService) {
 		this.productService = productService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ProductDto>> getAllProduct(){
+		return ResponseEntity.ok(productService.getAll());
 	}
 
 	@GetMapping("/{id}")
@@ -35,8 +42,25 @@ public class ProductResource {
 				.orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<ProductDto>> getAllProduct(){
-		return ResponseEntity.ok(productService.getAll());
+	// `http://localhost:8080/products?_page=1&_limit=8`
+	@GetMapping(params = {"_page", "_limit"})
+	public ResponseEntity<List<ProductDto>> getProductByPage(@RequestParam("_page") int page, @RequestParam("_limit") int limit){
+		return ResponseEntity.ok(productService.getProductByPage(page, limit));
 	}
+	
+	@GetMapping(params = {"q", "_page", "_limit"})
+	public ResponseEntity<List<ProductDto>> searchProduct(@RequestParam("q") String search, @RequestParam("_page") int page, @RequestParam("_limit") int limit){
+		return ResponseEntity.ok(productService.searchProduct(search, page, limit));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductDto> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto){
+		System.out.println("id: " + id);
+		System.out.println("updates: " + productDto);
+		productDto.setId(id);
+		return productService.update(productDto)?
+				ResponseEntity.ok(productDto):
+				ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	}
+
 }
