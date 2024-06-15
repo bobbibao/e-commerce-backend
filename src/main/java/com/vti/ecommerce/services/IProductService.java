14043d717f1b1e1943e4e595d61c3fb54a635680
bobbibao <lehoangbao1125@gmail.com>
2024-06-15 14:46:17 +0700

    Implement get Product by page and search product

--- src/main/java/com/vti/ecommerce/services/IProductService.java
+++ src/main/java/com/vti/ecommerce/services/IProductService.java
@@ -1,11 +1,15 @@
 package com.vti.ecommerce.services;
 
+import java.util.List;
+
 import com.vti.ecommerce.domains.entities.Product;
 import com.vti.ecommerce.services.dto.ProductDto;
 
 public interface IProductService extends IBaseService<ProductDto, Long>{
-//    Product getProduct(Long id);
+   Product getProduct(Long id);
 //    ProductDto createProduct(ProductDto productDto);
 //    ProductDto updateProduct(Long id, ProductDto productDto);
 //    boolean deleteProduct(Long id);
+    List<ProductDto> getProductByPage(int page, int limit);
+    List<ProductDto> searchProduct(String search, int page, int limit);
 }
--- src/main/java/com/vti/ecommerce/services/impl/ProductServiceImpl.java
+++ src/main/java/com/vti/ecommerce/services/impl/ProductServiceImpl.java
@@ -1,10 +1,9 @@
 package com.vti.ecommerce.services.impl;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ import com.vti.ecommerce.domains.entities.Product;
 import com.vti.ecommerce.repositories.IProductRepository;
 import com.vti.ecommerce.services.IProductService;
 import com.vti.ecommerce.services.dto.ProductDto;
+import com.vti.ecommerce.services.dto.ReviewDto;
 
 @Service
 public class ProductServiceImpl implements IProductService {
@@ -27,13 +27,49 @@ public class ProductServiceImpl implements IProductService {
 
 	@Override
 	public boolean insert(ProductDto s) {
+		try {
+			Product product = new Product();
+			product.setProductCode(s.getProductCode());
+			product.setProductName(s.getName());
+			product.setDescription(s.getDescription());
+			product.setInStock(s.isInStock());
+			product.setRating(s.getRating());
+			product.setBrandName(s.getBrandName());
+			product.setImageURL(s.getImageUrl());
+			product.setCreateAt(s.getProductionDate());
+			product.setAvailableSize(s.getAvailableSizes());
+			product.setAdditionalImageURLs(s.getAdditionalImageUrls());
+			// product.setCategory(s.getCategory());
+			this.productRepository.save(product);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 
-		return this.productRepository.save(this.convertToEntity(s)) != null;
 	}
 
 	@Override
 	public boolean update(ProductDto s) {
-		return this.productRepository.save(this.convertToEntity(s)) != null;
+		try {
+			Product product = this.productRepository.findById(s.getId()).get();
+			product.setProductCode(s.getProductCode());
+			product.setProductName(s.getName());
+			product.setDescription(s.getDescription());
+			// product.setInStock(s.isInStock());
+			product.setRating(s.getRating());
+			product.setBrandName(s.getBrandName());
+			product.setImageURL(s.getImageUrl());
+			product.setCreateAt(s.getProductionDate());
+			// product.setAvailableSize(s.getAvailableSizes());
+			// product.setAdditionalImageURLs(s.getAdditionalImageUrls());
+			// product.setCategory(s.getCategory());
+			this.productRepository.save(product);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
@@ -53,13 +89,40 @@ public class ProductServiceImpl implements IProductService {
 	public Optional<ProductDto> getById(Long p) {
 		return this.productRepository.findById(p).map(this::convertToDto);
 	}
-
 	
 
 	@Override
 	public boolean updatePartial(Long p, Map<String, Object> updates) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			Product product = this.productRepository.findById(p).get();
+			updates.forEach((key, value) -> {
+				switch (key) {
+				case "name" -> product.setProductName((String) value);
+				case "description" -> product.setDescription((String) value);
+				case "inStock" -> product.setInStock((boolean) value);
+				case "rating" -> product.setRating((int) value);
+				case "brandName" -> product.setBrandName((String) value);
+				case "imageUrl" -> product.setImageURL((String) value);
+				case "productionDate" -> product.setCreateAt((LocalDate) value);
+				case "availableSizes" -> {
+                                }
+				case "additionalImageUrls" -> {
+                                }
+				case "category" -> {
+                                }
+					
+				default -> {
+                                }
+				}
+                            // product.setAvailableSize((int) value);
+                            // product.setAdditionalImageURLs((String) value);
+                            // product.setCategory((String) value);
+                            			});
+			this.productRepository.save(product);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
@@ -74,16 +137,35 @@ public class ProductServiceImpl implements IProductService {
 			return false;
 		}
 	}
+
 	private ProductDto convertToDto(Product product) {
 		ProductDto productDto = new ProductDto();
-		productDto.setProductID(product.getProductID());
-		productDto.setProductName(product.getProductName());
+		productDto.setId(product.getProductID());
+		productDto.setProductCode(product.getProductCode());
+		productDto.setName(product.getProductName());
 		productDto.setDescription(product.getDescription());
 		productDto.setInStock(product.isInStock());
 		productDto.setRating(product.getRating());
 		productDto.setBrandName(product.getBrandName());
-		productDto.setImageURL(product.getImageURL());
-		// productDto.setPrice(product.getPrices().get(0).getPriceValue());
+		productDto.setImageUrl(product.getImageURL());
+		productDto.setProductionDate(product.getCreateAt());
+		productDto.setAvailableSizes(product.getAvailableSize());
+		productDto.setTotalReviewCount(product.getCustomerReviews().size());
+		productDto.setAdditionalImageUrls(product.getAdditionalImageURLs());
+		productDto.setReviews(product.getCustomerReviews().stream().map(review -> {
+			ReviewDto reviewDto = new ReviewDto();
+			reviewDto.setUsername(review.getUser().getFirstName());
+			reviewDto.setUserImage(review.getUser().getUserImage());
+			reviewDto.setLocation(review.getUser().getAddress().getCity());
+			reviewDto.setRating(review.getRating());
+			reviewDto.setDate(review.getReviewDate().toString());
+			reviewDto.setReviewTitle(review.getReviewTitle());
+			reviewDto.setReviewText(review.getReviewText());
+			return reviewDto;
+		}).collect(Collectors.toList()));
+		productDto.setCategory(product.getCategory().name());
+		productDto.setGender(product.getGender().name());
+		
 		try{
 			double price = product.getPrices().get(0).getPriceValue();
 			productDto.setPrice(price);
@@ -92,21 +174,16 @@ public class ProductServiceImpl implements IProductService {
 		}
 		return productDto;
 	}
-	private Product convertToEntity(ProductDto productDto) {
-		Product product = new Product();
-		product.setProductID(productDto.getProductID());
-		product.setProductName(productDto.getProductName());
-		product.setDescription(productDto.getDescription());
-		product.setInStock(productDto.isInStock());
-		product.setRating(productDto.getRating());
-		product.setBrandName(productDto.getBrandName());
-		product.setImageURL(productDto.getImageURL());
-		return product;
-	}
 
 	@Override
 	public List<ProductDto> getAll() {
 		return this.productRepository.findAll().stream().map(this::convertToDto).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<ProductDto> getProductByPage(int page, int limit) {
+		return this.productRepository.findAll().stream().skip((page - 1) * limit).limit(limit).map(this::convertToDto)
+				.collect(Collectors.toList());
 	}
 
 	
@@ -131,4 +208,14 @@ public class ProductServiceImpl implements IProductService {
 //		// TODO Auto-generated method stub
 //		throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
 //	}
+
+    @Override
+    public Product getProduct(Long id) {
+        return productRepository.findById(id).get();
+    }
+
+    @Override
+    public List<ProductDto> searchProduct(String search, int page, int limit) {
+		return this.productRepository.findByProductNameContaining(search).stream().skip((page - 1) * limit).limit(limit).map(this::convertToDto).collect(Collectors.toList());
+    }
 }
