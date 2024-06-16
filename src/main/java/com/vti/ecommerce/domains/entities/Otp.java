357c8eda881e3c1dc348c3a00c52be256d74f367
bobbibao <lehoangbao1125@gmail.com>
2024-06-16 18:00:00 +0700

    Add Otp entity

---
+++ src/main/java/com/vti/ecommerce/domains/entities/Otp.java
@@ -0,0 +1,28 @@
+package com.vti.ecommerce.domains.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity 
+@Data
+public class Otp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String otp;
+
+    @Column(nullable = false)
+    private LocalDateTime expirationTime;
+
+    @Column(nullable = false)
+    private String email; 
+}
