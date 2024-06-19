733c136f327c7195b6f46fec6400434724043efb
bobbibao <lehoangbao1125@gmail.com>
2024-06-19 10:48:03 +0700

    Add config multipart file and add supplier repository

---
+++ src/main/java/com/vti/ecommerce/config/MultipartConfig.java
@@ -0,0 +1,20 @@
+package com.vti.ecommerce.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import jakarta.servlet.MultipartConfigElement;
+
+@Configuration
+public class MultipartConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.parse("10MB"));
+        factory.setMaxRequestSize(DataSize.parse("10MB"));
+        return factory.createMultipartConfig();
+    }
+}
---
+++ src/main/java/com/vti/ecommerce/repositories/ISupplierRepository.java
@@ -0,0 +1,16 @@
+package com.vti.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vti.ecommerce.domains.entities.Supplier;
+
+public interface ISupplierRepository extends JpaRepository<Supplier, Long> {
+    // Supplier findById(Long supplierID);
+    // SupplierDto findById(String supplierID);
+    // List<Supplier> findAll();
+    // boolean save(Supplier supplier);
+    // boolean deleteById(Long supplierID);
+    // List<Supplier> findBySupplierNameContaining(String search);
+    // List<Map<String, Object>> countSupplierByCategory();
+
+}
